3e037ec7755323f24ea666f50f2267e3eb65469f
BrianZ <zickbe@vt.edu>
2013-11-19 12:12:54 -0500

    simplistic chat implementation, fixed some bugs with xml

--- src/edu/vt/ece4564/finalproject/databridge/ChatFragment.java
+++ src/edu/vt/ece4564/finalproject/databridge/ChatFragment.java
@@ -14,59 +14,78 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class ChatFragment extends Fragment {
-EditText writeMessage;
-Button sendMessage;
-TextView viewMessage;
-ArrayList<String> messageList; // List of messages
-ChatInterface chatInterface;
+	EditText writeMessage;
+	Button sendMessage;
+	TextView viewMessage;
+	ArrayList<String> messageList; // List of messages
+	ChatInterface chatInterface;
 
 	/*
-	 * This allows for the fragment to have an interface with mainactivity
-	 * so that it can send chat messages
+	 * This allows for the fragment to have an interface with mainactivity so
+	 * that it can send chat messages
+	 * 
 	 * @see android.support.v4.app.Fragment#onAttach(android.app.Activity)
 	 */
 	@Override
 	public void onAttach(Activity activity) {
-	    super.onAttach(activity);
-	
-	    // This makes sure that the container activity has implemented
-	    // the callback interface. If not, it throws an exception
-	    try {
-	        chatInterface = (ChatInterface) activity;
-	    } catch (ClassCastException e) {
-	        throw new ClassCastException(activity.toString()
-	                + " must implement ServerFragmentInterface.");
-	    }
+		super.onAttach(activity);
+
+		// This makes sure that the container activity has implemented
+		// the callback interface. If not, it throws an exception
+		try {
+			chatInterface = (ChatInterface) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement ServerFragmentInterface.");
+		}
 	}
-	
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.chat_fragment, container, false);
-        messageList = new ArrayList<String>();
-        writeMessage = (EditText)rootView.findViewById(R.id.editText1);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.chat_fragment, container,
+				false);
+		messageList = new ArrayList<String>();
+		writeMessage = (EditText) rootView.findViewById(R.id.editText1);
 		sendMessage = (Button) rootView.findViewById(R.id.button1);
-		viewMessage = (TextView) rootView.findViewById(R.id.textView2); // Textview for all messages
-		
+		viewMessage = (TextView) rootView.findViewById(R.id.messageView); // Textview
+																			// for
+																			// all
+																			// messages
+
 		sendMessage.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO do checking for blank and other fun jazz
 				// Send the message using the interface
-				chatInterface.sendMessage(writeMessage.getText().toString()); 
+				String messageTyped = writeMessage.getText().toString();
+				if (!messageTyped.equals("")) 
+				{
+					chatInterface.sendMessage(messageTyped);
+					messageList.add("Client:" + messageTyped + "\n");
+					viewMessage.append("Client:" + messageTyped + "\n");
+					
+//					viewMessage.setText("");
+//					for (String s : messageList) {
+//						viewMessage.append(s);
+//						viewMessage.append("\n");
+//					}
+
+				}
+				writeMessage.setText("");
+
 			}
 		});
-        return rootView;
-    }
-	
+		return rootView;
+	}
+
 	/*
-	 * Get a new message and add it to the chat array
-	 * Call it from mainactivity
+	 * Get a new message and add it to the chat array Call it from mainactivity
 	 */
 	public void addMessage(String newMessage) {
 		messageList.add(newMessage); // Add the message to the messagelist
 		viewMessage.setText("");
-		for(String S: messageList) {
+		for (String S : messageList) {
 			viewMessage.append(S);
 		}
 		// TODO maybe something better idk I'm just doing this to debug
