d9998baa5362a9d81eb1c34951ca3516a59786af
rferrance <rferranc@vt.edu>
2013-11-19 13:57:30 -0500

    pretty terminal!

--- src/edu/vt/ece4564/finalproject/databridge/MainActivity.java
+++ src/edu/vt/ece4564/finalproject/databridge/MainActivity.java
@@ -36,6 +36,11 @@ public class MainActivity extends FragmentActivity
 	 * The {@link ViewPager} that will host the section contents.
 	 */
 	ViewPager mViewPager;
+	// Fragments to be used.
+	EmulatorFragment emulFragment;
+	TerminalFragment termFragment;
+	ChatFragment chatFragment;
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +58,6 @@ public class MainActivity extends FragmentActivity
 		Intent intent = new Intent(this, ConnectionActivity.class);
 		intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         startActivityForResult(intent, 1);
-        
         
 	}
 
